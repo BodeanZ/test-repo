649b5b2675803f20def4fcff97863d7fc71b4234
bodeanz <akunin1221@yandex.ru>
2016-12-16 20:40:29 +0200

    fatal error fixed in cycle

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 echo 'some text in branch feature/usefulFeature';
-while($i = 0; $i < 10; $i++)
+for($i = 0; $i < 10; $i++)
 	echo 'iteration ' . $i . '<br />';
 echo 'some changes';
 ?>
